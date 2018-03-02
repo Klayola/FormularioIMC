c53f1cb91d4d261473aee8e9246013ee3af40ff7
logonrm <logonrm@fiap.com.br>
2018-03-02 09:33:51 -0300

    terminamos o imc

--- app/src/main/java/com/example/logonrm/myapplication/ResultadoActivity.kt
+++ app/src/main/java/com/example/logonrm/myapplication/ResultadoActivity.kt
@@ -2,6 +2,9 @@ package com.example.logonrm.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import com.example.logonrm.myapplication.extencion.format
+import kotlinx.android.synthetic.main.activity_resultado.*
 
 class ResultadoActivity : AppCompatActivity() {
 
@@ -9,6 +12,37 @@ class ResultadoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resultado)
 
+        if (intent != null){
+            val peso = intent.getStringExtra("PESO").toDouble()
+            val altura = intent.getStringExtra("ALTURA").toDouble()
 
+            val imc = peso / (altura * altura)
+
+            tvValorIMC.setText(imc.format(1))
+
+            if (imc <= 18.5) {
+                alteraImagem(R.drawable.abaixo)
+                tvStatusIMC.text = "Abaixo do Peso"
+            }else if(imc <= 24.9){
+                alteraImagem(R.drawable.ideal)
+                tvStatusIMC.text = "Peso Ideal"
+            }else if(imc <= 29.9){
+                alteraImagem(R.drawable.sobre)
+                tvStatusIMC.text = "Acima do Peso"
+            }else if(imc <= 39.9){
+                alteraImagem(R.drawable.obesidade)
+                tvStatusIMC.text = "Obesidade I"
+            }else if(imc <= 49.9){
+                alteraImagem(R.drawable.obesidade)
+                tvStatusIMC.text = "Obesidade II (Mevera)"
+            }else{
+                alteraImagem(R.drawable.obesidade)
+                tvStatusIMC.text = "Obesidade II (Mórbida)"
+            }
+        }
+    }
+
+    fun alteraImagem(idImagem: Int) {
+        ivStatusIMC.setImageDrawable(ContextCompat.getDrawable(this, idImagem))
     }
 }
---
+++ app/src/main/java/com/example/logonrm/myapplication/extencion/DoubleExtension.kt
@@ -0,0 +1,6 @@
+package com.example.logonrm.myapplication.extencion
+
+/**
+ * Created by logonrm on 02/03/2018.
+ */
+fun Double.format(digits: Int) = java.lang.String.format("%.${digits}f", this)
